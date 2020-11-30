b6cbae8e14584095855a7a3d9c770224a43cfc3a
Totaku <banana@3lf.ru>
2020-11-30 23:28:23 +0300

    add pug

--- gulp/tasks/pug.js
+++ gulp/tasks/pug.js
@@ -5,12 +5,12 @@ const pug = require('gulp-pug');
 const argv = require('yargs').argv;
 const gulpif = require('gulp-if');
 
-// Преобразуем Pug в HTML
-
 module.exports = function pug2html() {
-  return gulp.src('dev/pug/*.pug')
+  return gulp.src(['src/views/*.pug', 'src/views/pages/*.pug'])
     .pipe(plumber())
-    .pipe(pug())
+    .pipe(pug({
+        pretty: true
+    }))
     .pipe(plumber.stop())
     .pipe(gulpif(argv.prod, htmlValidator()))
     .pipe(gulp.dest('dist'))
